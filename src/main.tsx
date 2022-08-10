58c6a50b51427a80ca93ca2ec7abab9e10145533
devrayat000 <2010050@me.buet.ac.bd>
2022-08-10 23:02:45 +0600

    implemented given theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MantineProvider, DEFAULT_THEME } from '@mantine/core';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<MantineProvider
+			withNormalizeCSS
+			withGlobalStyles
+			withCSSVariables={import.meta.env.PROD}
+			theme={{
+				colorScheme: 'light',
+				other: {
+					'almost-white': 'hsl(0, 0%, 98%)',
+					'medium-gray': 'hsl(0, 0%, 41%)',
+					'almost-black': 'hsl(0, 0%, 8%)'
+				},
+				fontFamily: "'Epilogue', " + DEFAULT_THEME.fontFamily
+			}}
+		>
+			<App />
+		</MantineProvider>
 	</React.StrictMode>
 );
